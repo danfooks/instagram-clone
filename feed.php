9e87bacbe0d5605463ab9a958f7d560f5767f8d3
Dan Fooks <46224768+danfooks@users.noreply.github.com>
2023-04-22 18:17:41 -0400

    add link for home page

--- feed.php
+++ feed.php
@@ -38,7 +38,7 @@ $userid = $_SESSION['userid'];
   <body>
     <nav class="navbar">
       <div class="nav-wrapper">
-        <img src="img/navLogo.PNG" class="brand-img" alt="" />
+        <a href="./feed.php"><img src="img/navLogo.PNG" class="brand-img" alt="" /></a>
         <input type="text" class="search-box" placeholder="search" />
         <div class="nav-items">
 
