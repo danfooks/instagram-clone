4b240d1444814556f574d0fd2150bfbc07344cda
Dan Fooks <46224768+danfooks@users.noreply.github.com>
2023-04-22 17:18:15 -0400

    update files and move css to one folder

--- feed.php
+++ feed.php
@@ -33,7 +33,7 @@ $userid = $_SESSION['userid'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Instagram</title>
     <link rel="icon" type="image/x-icon" href="img/logo.png" />
-    <link rel="stylesheet" href="feed.css" />
+    <link rel="stylesheet" href="css/feed.css" />
   </head>
   <body>
     <nav class="navbar">
